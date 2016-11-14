ebf81ddead90b28b423d2e19aa027f3e7b77041a
Administrator <Administrator@daizhi-PC>
2016-11-14 10:41:36 +0800

    第一次测试git项目

---
+++ ssm/test/com/dz/test/Test.java
@@ -0,0 +1,40 @@
+package com.dz.test;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.dz.mybatis.entity.Person;
+
+public class Test {
+	private static SqlSessionFactory sqlSessionFactory;
+	private static Reader reader;
+
+	static {
+		try {
+			reader = Resources.getResourceAsReader("Configuration.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static SqlSessionFactory getSession() {
+		return sqlSessionFactory;
+	}
+
+	public static void main(String[] args) {
+		SqlSession session = sqlSessionFactory.openSession();
+		try {
+			Person person = (Person) session.selectOne(
+					"com.dz.mybatis.entity.PersonMapper.selectPersonByID", 1);
+			System.out.println(person.getAddress());
+			System.out.println(person.getName());
+		} finally {
+			session.close();
+		}
+	}
+}
