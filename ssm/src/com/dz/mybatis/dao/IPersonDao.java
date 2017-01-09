51808a4c20383e2bfa6a0483f527a3fe1605de4c
Administrator <Administrator@daizhi-PC>
2017-01-09 14:49:56 +0800

    git第二次提交

--- ssm/src/com/dz/mybatis/dao/IPersonDao.java
+++ ssm/src/com/dz/mybatis/dao/IPersonDao.java
@@ -1,6 +1,9 @@
 package com.dz.mybatis.dao;
 
 import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Select;
 
 import com.dz.mybatis.entity.Person;
 
@@ -9,5 +12,16 @@ public interface IPersonDao {
 	public Person selectPersonByID(int id);
 	
 	public List<Person> selectAllPersonMap();
+	
 	public List<Person> selectAllPersonType();
+	@Select("select * from person")
+	public List<Person> selectAllPersonAnnotation();
+	
+	public Map<String, Object> selectPersonById(int id);
+	
+	public boolean insertDB(Person p);
+	
+	public boolean updateDB(Person p);
+	
+	public boolean deleteDB(int id);
 }
--- ssm/test/com/dz/test/Test2.java
+++ ssm/test/com/dz/test/Test2.java
@@ -3,6 +3,7 @@ package com.dz.test;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -11,6 +12,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.dz.mybatis.dao.IPersonDao;
 import com.dz.mybatis.entity.Person;
+import com.mysql.jdbc.Connection;
 
 public class Test2 {
 	private static SqlSessionFactory sqlSessionFactory;
@@ -34,10 +36,26 @@ public class Test2 {
 		try {
 			IPersonDao personDao = session.getMapper(IPersonDao.class);
 			Person p = personDao.selectPersonByID(1);
-			System.out.println(p);
+			//System.out.println(p);
 			//List<Person> list = personDao.selectAllPersonMap();
-			List<Person> list = personDao.selectAllPersonType();
-			System.out.println(list);
+			//List<Person> list = personDao.selectAllPersonType();
+			//List<Person> list = personDao.selectAllPersonAnnotation();
+			//System.out.println(list);
+			//Map<String, Object> map = personDao.selectPersonById(1);
+			//System.out.println(map.get("name"));
+			//Person p1 = new Person();
+			//p1.setName("David");
+			//p1.setAge(25);
+			//p1.setAddress("China");
+			//boolean b = personDao.insertDB(p1);
+			//注意当插入或者修改或者删除时一定要提交
+//			Person p2 =new Person();
+//			p2.setName("Peter");
+//			p2.setId(10);
+//			personDao.updateDB(p2);
+//			personDao.deleteDB(3);
+//			session.commit();
+			
 		} finally {
 			session.close();
 		}
